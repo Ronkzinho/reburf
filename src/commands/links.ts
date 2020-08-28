b1552b1bc6737ff7b08b17ef16c5878038142d2a
ronkzinho <hfrcr@hotmail.com>
2020-08-27 23:18:25 -0300

    Links command added, and little changes

---
+++ src/commands/links.ts
@@ -0,0 +1,28 @@
+import { command, runCommand } from "../utils/command";
+import Embed from "../utils/Classes/Embed";
+
+export default abstract class extends command{
+    constructor(name, client){
+        super(name, client)
+        this.name = "links"
+        this.aliases = ["l"]
+        this.allowDm = true
+        this.category = "utility"
+        this.description = "Comando para que você possa ver os links relacionados ao bot!"
+        this.usage = [`${this.name}`]
+    }
+    async run({ message, pJ }: runCommand){
+        const fields = []
+        this.client.messages.commands.links.fields.map(c => {
+            var { inviteLink, gitHubRepository } = this.client
+            c.value = pJ(c.value, { inviteLink, gitHubRepository })
+            fields.push(c)
+        })
+        const embed = new Embed({
+            title: `Olá, ${this.client.owners.includes(message.author.id) ? "dono, como está seu dia?" : message.author.username + ", tudo bem?"}`,
+            description: `${pJ(this.client.messages.commands.links.desciption, { owner0: this.client.owners[0], owner1: this.client.owners[1] })}`,
+            fields
+        }, message)
+        message.channel.send(embed)
+    }
+}
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,6 +3,8 @@ import { connect } from "http2";
 
 export default async function(this: Reburf){
     console.log(this.user.username + " onlainer")
+    this.inviteLink = `https://discord.com/oauth2/authorize?client_id=${this.user.id}&scope=bot&permissions=8`
+    this.gitHubRepository = `https://github.com/${this.users.cache.get(this.owners[0]).username}/${this.user.username}/`
     this.filteredCommands = this.commands.filter(c => !c.hidden && !c.owner && !c.manutencion)
     this.user.setActivity({ name: `comandos com meu prefixo: ${this.prefix}`, type: 3 })
 }
