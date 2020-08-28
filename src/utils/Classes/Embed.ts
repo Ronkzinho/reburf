760bb3cb1ccaa0b86855f6944af220ed05b86935
ronkzinho <hfrcr@hotmail.com>
2020-08-28 01:12:43 -0300

    Small changes

--- src/utils/Classes/Embed.ts
+++ src/utils/Classes/Embed.ts
@@ -1,7 +1,7 @@
 import { MessageEmbed, Message, MessageEmbedOptions } from "discord.js";
 
 export default class Embed extends MessageEmbed{
-    constructor(options: MessageEmbedOptions = {}, message?: Message, type?:number){
+    constructor(options: MessageEmbedOptions = {}, message?: Message, type?: number){
         super(options)
         message && (type ? type === 3 || (type > 5 && type < 6) : true) && this.setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
         message && (type ? type === 2 || (type > 4 && type < 7): true) && this.setTimestamp()
